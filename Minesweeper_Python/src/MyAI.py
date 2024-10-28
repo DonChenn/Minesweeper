0179e34ba753552c1c7333f7894af568f5c3e8cc
DonChenn <donovanachen@gmail.com>
2024-10-27 23:05:15 -0700

    swapped x and y

--- Minesweeper_Python/src/MyAI.py
+++ Minesweeper_Python/src/MyAI.py
@@ -15,6 +15,7 @@
 from AI import AI
 from Action import Action
 from heapq import heappush, heappop
+import copy
 
 
 class MyAI(AI):
@@ -35,15 +36,15 @@ class MyAI(AI):
 
     def addActionsToQueue(self, priority, action, directions):
         for cell in directions:
-            new_x, new_y = cell
-            if self.inBounds(new_x, new_y) and (new_x, new_y) not in self.uncovered:
-                heappush(self.queue, (priority, (action, new_x, new_y)))
+            new_y, new_x = cell
+            if self.inBounds(new_y, new_x) and (new_y, new_x) not in self.uncovered:
+                heappush(self.queue, (priority, (action, new_y, new_x)))
                 if action == "UNCOVER":
-                    self.uncovered.add((new_x, new_y))
+                    self.uncovered.add((new_y, new_x))
                 elif action == "FLAG":
-                    self.bombs.add((new_x, new_y))
+                    self.bombs.add((new_y, new_x))
 
-    def inBounds(self, x, y):
+    def inBounds(self, y, x):
         return (0 <= x < self.row_dimension and
                 0 <= y < self.col_dimension)
 
@@ -51,9 +52,9 @@ class MyAI(AI):
     def checkUncovered(self, directions):
         uncovered = []
         for cell in directions:
-            new_x, new_y = cell
-            if self.inBounds(new_x, new_y) and (new_x, new_y) not in self.uncovered:
-                uncovered.append((new_x, new_y))
+            new_y, new_x = cell
+            if self.inBounds(new_y, new_x) and (new_y, new_x) not in self.uncovered:
+                uncovered.append((new_y, new_x))
         return uncovered
 
     # finds all bombs and remaining spaces
@@ -62,32 +63,32 @@ class MyAI(AI):
         potential_safes = []
         for cell in directions:
             new_x, new_y = cell
-            if self.inBounds(new_x, new_y) and (new_x, new_y) in self.bombs:
-                adj_bombs.append((new_x, new_y))
+            if self.inBounds(new_y, new_x) and (new_y, new_x) in self.bombs:
+                adj_bombs.append((new_y, new_x))
             else:
-                potential_safes.append((new_x, new_y))
+                potential_safes.append((new_y, new_x))
         return potential_safes, adj_bombs
 
     # returns list of tuples (x, y) of adjacent cells
-    def getAdjacentCells(self, x, y):
+    def getAdjacentCells(self, y, x):
         directions = [(-1, -1), (-1, 0), (-1, 1),
                       (0, -1), (0, 1),
                       (1, -1), (1, 0), (1, 1)]
         adj_cells = []
-        for dx, dy in directions:
-            new_x, new_y = x + dx, y + dy
-            if self.inBounds(new_x, new_y):
-                adj_cells.append((new_x, new_y))
+        for dy, dx in directions:
+            new_y, new_x = y + dy, x + dx
+            if self.inBounds(new_y, new_x):
+                adj_cells.append((new_y, new_x))
         return adj_cells
 
     # add queue to uncover all cells around 0
     def zero_action(self):
-        cells = self.getAdjacentCells(self.x, self.y)
+        cells = self.getAdjacentCells(self.y, self.x)
         self.addActionsToQueue(0, "UNCOVER", cells)
 
     # logic for current cell number and finding bombs around it
-    def hint_action(self, number, x, y, deferred=False):
-        directions = self.getAdjacentCells(x, y)
+    def hint_action(self, number, y, x, deferred=False):
+        directions = self.getAdjacentCells(y, x)
         potential_safes, bombs = self.checkBombs(directions)
         uncovered_cells = self.checkUncovered(directions)
 
@@ -100,22 +101,21 @@ class MyAI(AI):
         # come back to it for later
         else:
             if not deferred:
-                heappush(self.deferred_queue, (number, (x, y)))
+                heappush(self.deferred_queue, (number, (y, x)))
 
     # when on bomb cell, add adjacent cells to come back to
-    def bomb_action(self, number, x, y):
-        directions = self.getAdjacentCells(x, y)
+    def bomb_action(self, number, y, x):
+        directions = self.getAdjacentCells(y, x)
         for cell in directions:
-            new_x, new_y = cell
-            if (number, (new_x, new_y)) not in self.deferred_queue and (new_x, new_y) in self.uncovered:
-                print(f'Added to heap deferred {x, y, number, (new_x, new_y)}')
-                heappush(self.deferred_queue, (number, (new_x, new_y)))
+            new_y, new_x = cell
+            if (number, (new_y, new_x)) not in self.deferred_queue and (new_y, new_x) in self.uncovered:
+                heappush(self.deferred_queue, (number, (new_y, new_x)))
 
     def runQueue(self, queue):
         if queue:
             priority, cell = heappop(queue)
             # Update x, y to next cell
-            self.x, self.y = cell[1], cell[2]
+            self.y, self.x = cell[1], cell[2]
 
             # Execute action and return it
             if cell[0] == "UNCOVER":
@@ -128,11 +128,14 @@ class MyAI(AI):
 
     def getAction(self, number: int) -> "Action Object":
         # Update the board
-        self.board[self.x][self.y] = number
-        print(f'CURRENT CELL: {self.x, self.y}')
+        self.board[self.y][self.x] = number
+        print(f'CURRENT CELL: {self.y, self.x}')
         print(f'NUMBER: {number}')
-        for i, row in enumerate(self.board):
+        copied_list = copy.deepcopy(self.board)[::-1]
+        i = 7
+        for row in copied_list:
             print(i, row)
+            i -= 1
         print(f'QUEUE: {self.queue}')
         print(f'DEFERRED QUEUE: {self.deferred_queue}')
         print()
@@ -141,24 +144,23 @@ class MyAI(AI):
         if number == 0:
             self.zero_action()
         elif number > 0:
-            self.hint_action(number, self.x, self.y)
+            self.hint_action(number, self.y, self.x)
         else:
-            self.bomb_action(number, self.x, self.y)
+            self.bomb_action(number, self.y, self.x)
 
         action = self.runQueue(self.queue)
         if action:
             return action
 
         # Process queues
-        for revisit_cells in self.deferred_queue:
-            # Process the deferred queue
-            deferred_number, (x, y) = heappop(self.deferred_queue)
-            self.x, self.y = x, y
-            self.hint_action(deferred_number, x, y, True)
+        while not self.queue and self.deferred_queue:
+            deferred_number, (y, x) = heappop(self.deferred_queue)
+            self.y, self.x = y, x
+            self.hint_action(deferred_number, y, x, True)
 
-        action = self.runQueue(self.queue)
-        if action:
-            return action
+            action = self.runQueue(self.queue)
+            if action:
+                return action
 
         # No actions left
         return Action(AI.Action.LEAVE)
